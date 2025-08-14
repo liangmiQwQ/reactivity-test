eb2bfb07178047cbc196ee75ac398ea2d09ef3c4
liangmiQwQ <mi_yyds@126.com>
2025-08-14 18:54:03 +0800

    feat: init

---
+++ app/pages/bug.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ data }}
+    <button @click="data!.age = 2">Change Data</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { data } = await useAsyncData('mock', async () => {
+  // 这里直接返回伪造数据
+  return { name: 'kapa', age: 3 }
+})
+</script>
+
+<style>
+
+</style>
---
+++ app/pages/fix.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{ processedData }}
+    <button @click="processedData!.age = 2">Change Data</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { data } = await useAsyncData('mock', async () => {
+  // 这里直接返回伪造数据
+  return { name: 'kapa', age: 3 }
+})
+
+const processedData = ref();
+
+watch(data, (newValue) => {
+  processedData.value = newValue
+  // 乐观更新实践
+})
+</script>
+
+<style>
+
+</style>
